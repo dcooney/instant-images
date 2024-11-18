f364924e4cded3439496c97521e9a1d0d617d426
Darren Cooney <darren.cooney@gmail.com>
2024-11-18 11:41:56 -0500

    Revert testmode

--- build/plugin-sidebar/index.asset.php
+++ build/plugin-sidebar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'f56c1d4ad58d7184161a');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1e99a8e71c51efbc4271');
